4073ea80a37819000c505ba0989b381f14142e16
Sean Mayer <2008sean@live.com>
2022-06-09 05:55:24 +0100

    Adding Http Methods

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -1,10 +1,33 @@
 package com.appsdeveloperblog.app.ws.ui.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("users") // http://localhost:8080/users
 public class UserController {
- 
+
+  @GetMapping
+  public String getUser() {
+    return "Get user was called";
+  }
+
+  @PostMapping
+  public String createUser() {
+    return "Create user was called";
+  }
+
+  @PutMapping
+  public String updateUser() {
+    return "Update user was called";
+  }
+
+  @DeleteMapping
+  public String deleteUser() {
+    return "Delete user was called";
+  }
 }
