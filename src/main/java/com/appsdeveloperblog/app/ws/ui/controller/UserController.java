6785c41dd9b68741079b85fe6e150241b9b852d2
Sean Mayer <2008sean@live.com>
2022-06-09 05:49:25 +0100

    Create User Rest Controller class

---
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.appsdeveloperblog.app.ws.ui.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users") // http://localhost:8080/users
+public class UserController {
+ 
+}
