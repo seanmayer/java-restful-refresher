658ade086d94a1d1b65536678b0b503fa97342e6
Sean Mayer <2008sean@live.com>
2022-06-13 06:05:51 +0100

    User Request Model Class

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -4,15 +4,18 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.appsdeveloperblog.app.ws.ui.model.request.UserDetailsRequestModel;
 
 @RestController
 @RequestMapping("/users") // http://localhost:8080/users
 public class UserController {
 
   @GetMapping(value="")
-  public String getUser() {
+  public String getUser(@RequestBody UserDetailsRequestModel userDetails) {
     return "Get user was called";
   }
 
