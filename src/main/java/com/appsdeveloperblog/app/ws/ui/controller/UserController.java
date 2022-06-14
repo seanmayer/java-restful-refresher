0c374cc1ec210bc65bb0aae6b2d41b44e60a2c2a
Sean Mayer <2008sean@live.com>
2022-06-14 06:39:17 +0100

    User Service class methods

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.appsdeveloperblog.app.ws.ui.controller;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,20 +10,35 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.appsdeveloperblog.app.ws.service.UserService;
+import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
 import com.appsdeveloperblog.app.ws.ui.model.request.UserDetailsRequestModel;
+import com.appsdeveloperblog.app.ws.ui.model.response.UserRest;
+
 
 @RestController
 @RequestMapping("/users") // http://localhost:8080/users
 public class UserController {
 
+ @Autowired
+ UserService userService;
+
   @GetMapping(value="")
-  public String getUser(@RequestBody UserDetailsRequestModel userDetails) {
+  public String getUser() {
     return null;
   }
 
   @PostMapping(value="")
-  public String createUser() {
-    return "Create user was called";
+  public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
+    UserRest returnValue = new UserRest();
+
+    UserDto userDto = new UserDto();
+    BeanUtils.copyProperties(userDetails, userDto);
+
+    UserDto createdUser = userService.createUser(userDto);
+    BeanUtils.copyProperties(createdUser, returnValue);
+
+    return returnValue;
   }
 
   @PutMapping(value="")
