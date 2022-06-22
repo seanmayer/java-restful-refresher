f3d469874e2f2149a1b2babffa83edf88c04ea36
Sean Mayer <2008sean@live.com>
2022-06-22 06:42:04 +0100

    Preventing duplicate entries to database

--- src/main/java/com/appsdeveloperblog/app/ws/io/entity/UserEntity.java
+++ src/main/java/com/appsdeveloperblog/app/ws/io/entity/UserEntity.java
@@ -5,88 +5,93 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-@Entity(name="users")
+@Entity
+@Table(name = "users")
 public class UserEntity implements Serializable {
- private static final long serialVersionUID = -89898989898989898L;
- 
- @Id
- @GeneratedValue
- private long id;
 
- @Column(nullable=false, unique=true)
- private String userId;
+  private static final long serialVersionUID = -89898989898989898L;
 
- @Column(nullable=false, length=50)
- private String firstName;
+  @Id
+  @GeneratedValue
+  private long id;
 
- @Column(nullable=false, length=50)
- private String lastName;
+  @Column(nullable = false)
+  private String userId;
 
- @Column(nullable=false, length=120)
- private String email;
+  @Column(nullable = false, length = 50)
+  private String firstName;
 
- @Column(nullable=false)
- private String encryptPassword;
+  @Column(nullable = false, length = 50)
+  private String lastName;
 
- private String emailVerificationToken;
+  @Column(nullable = false, length = 120, unique = true)
+  private String email;
 
-@Column(nullable=false)
- private Boolean emailVerificationStatus = false;
+  @Column(nullable = false)
+  private String encryptPassword;
 
- public String getUserId() {
-  return userId;
+  private String emailVerificationToken;
+
+  @Column(nullable = false)
+  private Boolean emailVerificationStatus = false;
+
+  public String getUserId() {
+    return userId;
+  }
+
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getEncryptPassword() {
+    return encryptPassword;
+  }
+
+  public void setEncryptPassword(String encryptPassword) {
+    this.encryptPassword = encryptPassword;
+  }
+
+  public String getEmailVerificationToken() {
+    return emailVerificationToken;
+  }
+
+  public void setEmailVerificationToken(String emailVerificationToken) {
+    this.emailVerificationToken = emailVerificationToken;
+  }
+
+  public Boolean getEmailVerificationStatus() {
+    return emailVerificationStatus;
+  }
+
+  public void setEmailVerificationStatus(Boolean emailVerificationStatus) {
+    this.emailVerificationStatus = emailVerificationStatus;
+  }
+
+  
  }
-
- public void setUserId(String userId) {
-  this.userId = userId;
- }
-
- public String getFirstName() {
-  return firstName;
- }
-
- public void setFirstName(String firstName) {
-  this.firstName = firstName;
- }
-
- public String getLastName() {
-  return lastName;
- }
-
- public void setLastName(String lastName) {
-  this.lastName = lastName;
- }
-
- public String getEmail() {
-  return email;
- }
-
- public void setEmail(String email) {
-  this.email = email;
- }
-
- public String getEncryptPassword() {
-  return encryptPassword;
- }
-
- public void setEncryptPassword(String encryptPassword) {
-  this.encryptPassword = encryptPassword;
- }
-
- public String getEmailVerificationToken() {
-  return emailVerificationToken;
- }
-
- public void setEmailVerificationToken(String emailVerificationToken) {
-  this.emailVerificationToken = emailVerificationToken;
- }
-
- public Boolean getEmailVerificationStatus() {
-  return emailVerificationStatus;
- }
-
- public void setEmailVerificationStatus(Boolean emailVerificationStatus) {
-  this.emailVerificationStatus = emailVerificationStatus;
- }
-}
