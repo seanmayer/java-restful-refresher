df928ca37cd78766d6999caf8b9792632a2d8aea
Sean Mayer <2008sean@live.com>
2022-06-28 07:15:24 +0100

    AuthenticationFilter added

--- src/main/java/com/appsdeveloperblog/app/ws/security/SecurityConstants.java
+++ src/main/java/com/appsdeveloperblog/app/ws/security/SecurityConstants.java
@@ -5,5 +5,5 @@ public class SecurityConstants {
  public static final String TOKEN_PREFIX = "Bearer ";
  public static final String HEADER_STRING = "Authorization";
  public static final String SIGN_UP_URL = "/users";
- public static final String SECRET = "SecretKeyToGenereateJWTs";
+ public static final String SECRET = "jrgergwgrewg45gdgd";
 }
--- src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
+++ src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
@@ -32,7 +32,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
       .antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
       .permitAll()
       .anyRequest()
-      .authenticated();
+      .authenticated().and().addFilter(new AuthenticationFilter(authenticationManager()));
 
   }
 
