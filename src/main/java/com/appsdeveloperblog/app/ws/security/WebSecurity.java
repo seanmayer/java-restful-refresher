c0a2bf172208c7253fb3e950984e4c2117770aec
Sean Mayer <2008sean@live.com>
2022-07-18 07:03:10 +0100

    Add stateless - no caching app key on new requests

--- src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
+++ src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
@@ -34,7 +35,9 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
       .authenticated()
       .and()
       .addFilter(getAuthenticationFilter())
-      .addFilter(new AuthorizationFilter(authenticationManager()));
+      .addFilter(new AuthorizationFilter(authenticationManager()))
+      .sessionManagement()
+      .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
   }
 
   @Override
