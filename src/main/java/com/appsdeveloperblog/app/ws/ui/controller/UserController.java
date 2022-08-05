aa568b1406890f7e1e9112a73b88ce881e6c2de5
Sean Mayer <2008sean@live.com>
2022-08-05 06:07:26 +0100

    Fixed pagination issue

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -116,11 +116,11 @@ public class UserController {
   }
 
   @GetMapping(produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
-  public List<UserRest> getUsers(@RequestParam(value = "page", defaultValue = "1") int page,
+  public List<UserRest> getUsers(@RequestParam(value = "page", defaultValue = "0") int page,
     @RequestParam(value = "limit", defaultValue = "25") int limit) {
 
     List<UserRest> returnValue = new ArrayList<>();
-    List<UserDto> users = UserService.getUsers(page,limit);
+    List<UserDto> users = userService.getUsers(page, limit);
 
     for (UserDto userDto : users) {
       UserRest userRest = new UserRest();
