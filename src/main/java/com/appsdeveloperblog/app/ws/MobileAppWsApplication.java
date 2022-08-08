a68c26eeeff83c2e1960a330fec40bab374bf2c4
Sean Mayer <2008sean@live.com>
2022-08-08 08:13:33 +0100

    configure maven to create WAR package for deploying on existing tomcat containers instead of jar

--- src/main/java/com/appsdeveloperblog/app/ws/MobileAppWsApplication.java
+++ src/main/java/com/appsdeveloperblog/app/ws/MobileAppWsApplication.java
@@ -2,6 +2,8 @@ package com.appsdeveloperblog.app.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,7 +12,13 @@ import com.appsdeveloperblog.app.ws.security.AppProperties;
 
 @SpringBootApplication
 @ComponentScan({"com.appsdeveloperblog.app.ws"})
-public class MobileAppWsApplication {
+public class MobileAppWsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MobileAppWsApplication.class);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(MobileAppWsApplication.class, args);
 	}
