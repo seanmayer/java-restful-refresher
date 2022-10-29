50e4d2b0f62378a315ece9007daef2c6e7b7c267
Sean Mayer <2008sean@live.com>
2022-10-29 15:52:32 +0100

    Formatted

--- src/main/java/com/appsdeveloperblog/app/ws/security/SecurityConstants.java
+++ src/main/java/com/appsdeveloperblog/app/ws/security/SecurityConstants.java
@@ -3,15 +3,18 @@ package com.appsdeveloperblog.app.ws.security;
 import com.appsdeveloperblog.app.ws.SpringApplicationContext;
 
 public class SecurityConstants {
- public static final long EXPIRATION_TIME = 864000000; // 10 days
- public static final String TOKEN_PREFIX = "Bearer ";
- public static final String HEADER_STRING = "Authorization";
- public static final String SIGN_UP_URL = "/users";
- public static final String VERIFICATION_EMAIL_URL = "/users/email-verification";
 
- public static String getTokenSecret() {
-  AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("appProperties");
-  return appProperties.getTokenSecret();
- }
+  public static final long EXPIRATION_TIME = 864000000; // 10 days
+  public static final String TOKEN_PREFIX = "Bearer ";
+  public static final String HEADER_STRING = "Authorization";
+  public static final String SIGN_UP_URL = "/users";
+  public static final String VERIFICATION_EMAIL_URL =
+    "/users/email-verification";
 
+  public static String getTokenSecret() {
+    AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean(
+      "appProperties"
+    );
+    return appProperties.getTokenSecret();
+  }
 }
