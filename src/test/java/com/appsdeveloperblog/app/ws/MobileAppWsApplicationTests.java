e9635f87be8f6bfd21155fce99901ed726f51016
Sean Mayer <apart-things.0p@icloud.com>
2022-12-01 07:17:27 +0000

    Implemented new JUnit 5 Test case

--- src/test/java/com/appsdeveloperblog/app/ws/MobileAppWsApplicationTests.java
+++ src/test/java/com/appsdeveloperblog/app/ws/MobileAppWsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.appsdeveloperblog.app.ws.mobileappws;
+package com.appsdeveloperblog.app.ws;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MobileAppWsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
---
+++ src/test/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImplTest.java
@@ -0,0 +1,17 @@
+package com.appsdeveloperblog.app.ws.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class UserServiceImplTest {
+
+  @BeforeEach
+  void setUp() throws Exception {}
+
+  @Test
+  final void testGetter() {
+    fail("Not yet implemented");
+  }
+}
