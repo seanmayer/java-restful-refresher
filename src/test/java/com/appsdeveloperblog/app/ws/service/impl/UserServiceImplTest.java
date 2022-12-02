e7233186eec19be50ed4c12dbf0ea38b258af1dd
Sean Mayer <apart-things.0p@icloud.com>
2022-12-02 07:50:07 +0000

    Mocking objects with Mockito

--- src/test/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImplTest.java
+++ src/test/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImplTest.java
@@ -1,17 +1,45 @@
 package com.appsdeveloperblog.app.ws.service.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
+import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
+import com.appsdeveloperblog.app.ws.io.repositories.UserRepository;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 public class UserServiceImplTest {
+
+  @InjectMocks
+  UserServiceImpl userService;
+
+  @Mock
+  UserRepository userRepository;
 
   @BeforeEach
   void setUp() throws Exception {}
 
   @Test
-  final void testGetter() {
-    fail("Not yet implemented");
+   void testGetUser() {
+    UserEntity userEntity = new UserEntity();
+    userEntity.setId(1L);
+    userEntity.setFirstName("Sean");
+    userEntity.setUserId("asdfghjkl");
+    userEntity.setEncryptPassword("qwertyuiop");
+
+    when(userRepository.findByEmail(anyString())).thenReturn(userEntity);
+  }
+
+  @AfterEach
+  void tearDown() throws Exception {
   }
 }
