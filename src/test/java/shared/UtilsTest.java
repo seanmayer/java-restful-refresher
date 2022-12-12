1e555708341f8df06cf4d4ab0e5d1e79ee866114
Sean Mayer <apart-things.0p@icloud.com>
2022-12-12 07:04:01 +0000

    JUnit - Testing generateUserId

--- src/test/java/shared/UtilsTest.java
+++ src/test/java/shared/UtilsTest.java
@@ -1,26 +1,37 @@
 package shared;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.appsdeveloperblog.app.ws.shared.Utils;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
+@SpringBootTest(classes = {Utils.class})
 public class UtilsTest {
 
+ @Autowired
+ Utils utils;
+
  @BeforeEach
- void setUp() throws Exception {
+ void setUp() {
  }
 
  @Test
  void testGenerateUserId() {
-  // fail("Not yet implemented");
-
+  String userId = utils.generatedUserId(30);
+  String userId2 = utils.generatedUserId(30);
+  assertNotNull(userId);
+  assertNotNull(userId2);
+  assertNotNull(userId.length() == 30);
+  assertNotNull(userId2.length() == 30);
+  assertNotNull(!userId.equalsIgnoreCase(userId2));
  }
 
  @Test
