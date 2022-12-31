e5356ad91410cf2d58986fbb9795c981a69ddf3f
Sean Mayer <apart-things.0p@icloud.com>
2022-12-31 09:46:33 +0000

    Native SQL Queries - SELECT Query for returning verified email addresses

--- src/main/java/com/appsdeveloperblog/app/ws/io/repositories/UserRepository.java
+++ src/main/java/com/appsdeveloperblog/app/ws/io/repositories/UserRepository.java
@@ -1,5 +1,8 @@
 package com.appsdeveloperblog.app.ws.io.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +13,10 @@ public interface UserRepository extends PagingAndSortingRepository<UserEntity, L
  UserEntity findByEmail(String email);
  UserEntity findByUserId(String userId);
  UserEntity findUserByEmailVerificationToken(String token);
+
+ @Query(value="select * from Users u where u.EMAIL_VERIFICATION_STATUS = 'true'",
+ countQuery = "select count(*) from Users u where u.EMAIL_VERIFICATION_STATUS = 'true'",
+ nativeQuery=true)
+ Page<UserEntity> findAllUsersWithConfirmedEmailAddress(Pageable pageableRequest);
+
 }
