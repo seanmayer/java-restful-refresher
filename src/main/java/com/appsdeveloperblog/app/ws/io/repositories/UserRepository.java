492f8b5eb9890fcc350ad40177729ffcfb9a085f
Sean Mayer <apart-things.0p@icloud.com>
2023-01-03 21:45:32 +0000

    Resolved unsatisfied dependency expressed through field "userRepository"

--- src/main/java/com/appsdeveloperblog/app/ws/io/repositories/UserRepository.java
+++ src/main/java/com/appsdeveloperblog/app/ws/io/repositories/UserRepository.java
@@ -1,22 +1,23 @@
 package com.appsdeveloperblog.app.ws.io.repositories;
 
+import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
 
-import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
+public interface UserRepository
+  extends PagingAndSortingRepository<UserEntity, Long> {
+  UserEntity findByEmail(String email);
+  UserEntity findByUserId(String userId);
+  UserEntity findUserByEmailVerificationToken(String token);
 
-@Repository
-public interface UserRepository extends PagingAndSortingRepository<UserEntity, Long> {
- UserEntity findByEmail(String email);
- UserEntity findByUserId(String userId);
- UserEntity findUserByEmailVerificationToken(String token);
-
- @Query(value="select * from Users u where u.EMAIL_VERIFICATION_STATUS = 'true'",
- countQuery = "select count(*) from Users u where u.EMAIL_VERIFICATION_STATUS = 'true'",
- nativeQuery=true)
- Page<UserEntity> findAllUsersWithConfirmedEmailAddress(Pageable pageableRequest);
-
+  @Query(
+    value = "select * from Users u where u.EMAIL_VERIFICATION_STATUS = 'true'",
+    countQuery = "select count(*) from Users u where u.EMAIL_VERIFICATION_STATUS = 'true'",
+    nativeQuery = true
+  )
+  Page<UserEntity> findAllUsersWithConfirmedEmailAddress(
+    Pageable pageableRequest
+  );
 }
--- src/test/java/com/appsdeveloperblog/app/ws/io/repositories/UserRepositoryTest.java
+++ src/test/java/com/appsdeveloperblog/app/ws/io/repositories/UserRepositoryTest.java
@@ -1,12 +1,13 @@
 package com.appsdeveloperblog.app.ws.io.repositories;
 
+import com.appsdeveloperblog.app.ws.io.entity.AddressEntity;
+import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
-
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,52 +18,55 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import com.appsdeveloperblog.app.ws.io.entity.AddressEntity;
-import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
-
-
 @ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class UserRepositoryTest {
- 
- @Autowired
- UserRepository userRepository;
 
- @BeforeEach
- void setUp() throws Exception {
-  UserEntity userEntity = new UserEntity();
-  userEntity.setFirstName("Sean");
-  userEntity.setLastName("Mayer");
-  userEntity.setUserId("asdfghjkl");
-  userEntity.setEncryptPassword("asdfghjkl");
-  userEntity.setEmail("test@test.com");
-  userEntity.setEmailVerificationStatus(true);
+  @Autowired
+  UserRepository userRepository;
 
-  AddressEntity addressEntity = new AddressEntity();
-  addressEntity.setType("shipping");
-  addressEntity.setAddressId("asdfghjkl");
-  addressEntity.setCity("New York");
-  addressEntity.setCountry("USA");
-  addressEntity.setPostalCode("12345");
-  addressEntity.setStreetName("123 Street Name");
+  static boolean recordsCreated = false;
 
-  List<AddressEntity> addresses = new ArrayList<>();
-  addresses.add(addressEntity);
+  @BeforeEach
+  void setUp() throws Exception {
+    if (!recordsCreated) createRecords();
+  }
 
-  userEntity.setAddresses(addresses);
+  @Test
+  void testGetVerifiedUsers() {
+   Pageable pageableRequest = PageRequest.of(1, 1);
+   Page<UserEntity> page = userRepository.findAllUsersWithConfirmedEmailAddress(pageableRequest);
+   assertNotNull(page);
+   
+         List<UserEntity> userEntities = page.getContent();
+         assertNotNull(userEntities);
+         assertTrue(userEntities.size() == 1);
+  }
 
-  userRepository.save(userEntity);
+  private void createRecords() {
+    UserEntity userEntity = new UserEntity();
+    userEntity.setFirstName("Sean");
+    userEntity.setLastName("Mayer");
+    userEntity.setUserId("asdfghjkl");
+    userEntity.setEncryptPassword("asdfghjkl");
+    userEntity.setEmail("test@test.com");
+    userEntity.setEmailVerificationStatus(true);
 
- }
+    AddressEntity addressEntity = new AddressEntity();
+    addressEntity.setType("shipping");
+    addressEntity.setAddressId("asdfghjkl");
+    addressEntity.setCity("New York");
+    addressEntity.setCountry("USA");
+    addressEntity.setPostalCode("12345");
+    addressEntity.setStreetName("123 Street Name");
 
- @Test
- void testGetVerifiedUsers() {
-  Pageable pageableRequest = PageRequest.of(0, 2);
-  Page<UserEntity> page = userRepository.findAllUsersWithConfirmedEmailAddress(pageableRequest);
-  //assertNotNull(page);
+    List<AddressEntity> addresses = new ArrayList<>();
+    addresses.add(addressEntity);
 
-  //List<UserEntity> userEntities = page.getContent();
-  //assertNotNull(userEntities);
-  //assertTrue(userEntities.size() == 1);
- }
+    userEntity.setAddresses(addresses);
 
+    userRepository.save(userEntity);
+
+    recordsCreated = true;
+  }
 }
