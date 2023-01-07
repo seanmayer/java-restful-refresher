e11c01e6d6185c13f0aa00dbe1bd6328a09e0f6e
Sean Mayer <apart-things.0p@icloud.com>
2023-01-07 20:19:38 +0000

    Swagger - Create Swagger Config file

---
+++ src/main/java/com/appsdeveloperblog/app/ws/SwaggerConfig.java
@@ -0,0 +1,11 @@
+package com.appsdeveloperblog.app.ws;
+
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+ 
+}
