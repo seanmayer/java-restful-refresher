bb9d6a3265f3b08a085b0a15c36fdabcab05f677
Sean Mayer <apart-things.0p@icloud.com>
2023-01-17 06:54:01 +0000

    Spring Secruity Roles and Authorities - Added Authority Entity

---
+++ src/main/java/com/appsdeveloperblog/app/ws/io/entity/AuthorityEntity.java
@@ -0,0 +1,30 @@
+package com.appsdeveloperblog.app.ws.io.entity;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "authorities")
+public class AuthorityEntity implements Serializable {
+
+  private static final long serialVersionUID = -89898989898989898L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @Column(nullable = false, length = 20)
+  private String name;
+
+  @ManyToMany(mappedBy = "authorities")
+  private Collection<RoleEntity> roles;
+}
