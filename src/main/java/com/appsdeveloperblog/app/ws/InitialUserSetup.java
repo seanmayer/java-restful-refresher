ed02fc7ef5869036f3ca118983c782593fec55dc
Sean Mayer <apart-things.0p@icloud.com>
2023-01-18 07:03:01 +0000

    Spring Secruity Roles and Authorities - Created User with Admin role

--- src/main/java/com/appsdeveloperblog/app/ws/InitialUserSetup.java
+++ src/main/java/com/appsdeveloperblog/app/ws/InitialUserSetup.java
@@ -2,13 +2,18 @@ package com.appsdeveloperblog.app.ws;
 
 import com.appsdeveloperblog.app.ws.io.entity.AuthorityEntity;
 import com.appsdeveloperblog.app.ws.io.entity.RoleEntity;
+import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
 import com.appsdeveloperblog.app.ws.io.repositories.AuthorityRepository;
 import com.appsdeveloperblog.app.ws.io.repositories.RoleRepository;
+import com.appsdeveloperblog.app.ws.io.repositories.UserRepository;
+import com.appsdeveloperblog.app.ws.shared.Utils;
+
 import java.util.List;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -20,6 +25,15 @@ public class InitialUserSetup {
   @Autowired
   private RoleRepository roleRepository;
 
+  @Autowired
+  private Utils utils;
+
+  @Autowired
+  private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+  @Autowired
+  private UserRepository UserRepository;
+
   @EventListener
   @Transactional
   public void onApplicationEvent(ApplicationReadyEvent event) {
@@ -29,11 +43,28 @@ public class InitialUserSetup {
     AuthorityEntity writeAuthority = createAuthority("WRITE_AUTHORITY");
     AuthorityEntity deleteAuthority = createAuthority("DELETE_AUTHORITY");
 
-    createRole("ROLE_USER", List.of(readAuthority, writeAuthority));
-    createRole(
+    RoleEntity roleUser = createRole(
+      "ROLE_USER",
+      List.of(readAuthority, writeAuthority)
+    );
+
+    RoleEntity roleAdmin = createRole(
       "ROLE_ADMIN",
       List.of(readAuthority, writeAuthority, deleteAuthority)
     );
+
+    if (roleAdmin == null) return;
+
+    UserEntity adminUser = new UserEntity();
+    adminUser.setFirstName("Admin");
+    adminUser.setLastName("User");
+    adminUser.setEmail("admin@localhost");
+    adminUser.setEmailVerificationStatus(true);
+    adminUser.setUserId(utils.generatedUserId(30));
+    adminUser.setEncryptPassword(bCryptPasswordEncoder.encode("password"));
+    adminUser.setRoles(List.of(roleAdmin));
+    
+    UserRepository.save(adminUser);
   }
 
   @Transactional
