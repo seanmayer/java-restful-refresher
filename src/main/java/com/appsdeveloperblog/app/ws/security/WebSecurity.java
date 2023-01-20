334dedf946839de7460c755125880a2d1716ffc6
Sean Mayer <apart-things.0p@icloud.com>
2023-01-20 07:30:16 +0000

    Spring Secruity Roles and Authorities - Enabled Global Method security

--- src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
+++ src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
@@ -5,6 +5,7 @@ import com.appsdeveloperblog.app.ws.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,6 +15,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+@EnableGlobalMethodSecurity(securedEnabled = true)
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
